9c93f98c25f10842a32cbb3988496d0fef3d3056
xoventechdev <alkarim.doha@gmail.com>
2024-07-27 18:12:17 +0300

    report ongoing

--- src/components/report/ExpenseReport.jsx
+++ src/components/report/ExpenseReport.jsx
@@ -4,6 +4,16 @@ import { useSelector } from "react-redux";
 import { ToastContainer } from "react-toastify";
 import { ErrorToast } from "../../utility/FormHelper";
 
+import {
+  AreaChart,
+  Area,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
+
 const ExpenseReport = () => {
   const report = useSelector((state) => state.report.expenseReport);
   console.log(report);
@@ -20,6 +30,50 @@ const ExpenseReport = () => {
       ErrorToast("Please, select a date range");
     }
   };
+  const data = [
+    {
+      name: "Page A",
+      uv: 4000,
+      pv: 2400,
+      amt: 2400,
+    },
+    {
+      name: "Page B",
+      uv: 3000,
+      pv: 1398,
+      amt: 2210,
+    },
+    {
+      name: "Page C",
+      uv: 2000,
+      pv: 9800,
+      amt: 2290,
+    },
+    {
+      name: "Page D",
+      uv: 2780,
+      pv: 3908,
+      amt: 2000,
+    },
+    {
+      name: "Page E",
+      uv: 1890,
+      pv: 4800,
+      amt: 2181,
+    },
+    {
+      name: "Page F",
+      uv: 2390,
+      pv: 3800,
+      amt: 2500,
+    },
+    {
+      name: "Page G",
+      uv: 3490,
+      pv: 4300,
+      amt: 2100,
+    },
+  ];
   return (
     <div className="container-fluid">
       <div className="row">
@@ -83,6 +137,34 @@ const ExpenseReport = () => {
         ) : (
           <div></div>
         )}
+      </div>
+      <div className="row">
+        <div className="col-md-6 p-2">
+          <div className="card">
+            <div className="card-body">
+              <span className="h6">Expense Last 30 Days</span>
+              <ResponsiveContainer className="mt-4" width="100%" height={200}>
+                <AreaChart
+                  width={500}
+                  height={200}
+                  data={report.data}
+                  margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
+                >
+                  <CartesianGrid strokeDasharray="3 3" />
+                  <XAxis dataKey="_id" />
+                  <YAxis />
+                  <Tooltip />
+                  <Area
+                    type="monotone"
+                    dataKey="TotalAmount"
+                    stroke="#CB0C9F"
+                    fill="#CB0C9F"
+                  />
+                </AreaChart>
+              </ResponsiveContainer>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
